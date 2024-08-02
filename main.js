4cdb480ba903a8d953a9e4c48831b14b287a0f98
jasheloper <48870048+jasheloper@users.noreply.github.com>
2024-08-02 15:16:35 -0700

    mvp complete but also changed some of the css

--- main.js
+++ main.js
@@ -31,3 +31,14 @@ for (let i = 0; i < imgArr.length; i++) {
 
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener("click", () => {
+    if (btn.getAttribute("class") === "dark") {
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
+    } else {
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
+    }
+});
