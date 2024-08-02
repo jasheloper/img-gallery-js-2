8b75ceb864bdca995f123d037d2a71113d656540
jasheloper <48870048+jasheloper@users.noreply.github.com>
2024-08-02 14:38:49 -0700

    gallery wip

--- main.js
+++ main.js
@@ -5,14 +5,29 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imgArr = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+const imgAlts = [
+    {altText: "Photo 1"},
+    {altText: "Photo 2"},
+    {altText: "Photo 3"},
+    {altText: "Photo 4"},
+    {altText: "Photo 5"}
+  ];
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 0; i < imgArr.length; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${imgArr[i]}`);
+    newImage.setAttribute('alt', imgAlts[i].altText);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener("click", () => {
+        displayedImage.setAttribute('src', newImage.getAttribute('src'));
+        displayedImage.setAttribute('alt', newImage.getAttribute('alt'));
+    });
+}
+
 
 /* Wiring up the Darken/Lighten button */
